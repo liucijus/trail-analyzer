16f82a82ae3d82037160670a747da0076ef94db4
vaidas <vaidas.pilkauskas@gmail.com>
2014-01-28 22:03:15 +0200

    fixed track-parser version; added local dependency resolver

--- project/Build.scala
+++ project/Build.scala
@@ -10,10 +10,11 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     jdbc,
     anorm,
-    "lt.overdrive" %% "track-parser" % "1.0-b659bae8183a7c5a43b4da44bc26bc6f4c55414e"
+    "lt.overdrive" %% "track-parser" % "1.0-629582bddca736e03ff282fa04fbd16f6ef072fa"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    resolvers += Resolver.url("lt.overdrive", url("http://liucijus.github.io/track-parser-scala/"))(Resolver.ivyStylePatterns)
+    resolvers += Resolver.url("lt.overdrive", url("http://liucijus.github.io/track-parser-scala/"))(Resolver.ivyStylePatterns),
+    resolvers += Resolver.file("local", file(System.getProperty("user.home") + "/.ivy2/local"))(Resolver.ivyStylePatterns)
   )
 }
