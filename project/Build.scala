ae6e294e48d64caebcaa73070026323dfa3952f8
vaidas <vaidas.pilkauskas@gmail.com>
2014-02-02 17:07:41 +0200

    track-parser dynamic dependency

--- project/Build.scala
+++ project/Build.scala
@@ -10,11 +10,11 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     jdbc,
     anorm,
-    "lt.overdrive" %% "track-parser" % "1.0-629582bddca736e03ff282fa04fbd16f6ef072fa"
+    "lt.overdrive" %% "track-parser" % "1.0-+"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    resolvers += Resolver.url("lt.overdrive", url("http://liucijus.github.io/track-parser-scala/"))(Resolver.ivyStylePatterns),
-    resolvers += Resolver.file("local", file(System.getProperty("user.home") + "/.ivy2/local"))(Resolver.ivyStylePatterns)
+    resolvers += Resolver.file("local", file(System.getProperty("user.home") + "/.ivy2/local"))(Resolver.ivyStylePatterns),
+    resolvers += Resolver.url("lt.overdrive", url("http://liucijus.github.io/track-parser-scala/"))(Resolver.ivyStylePatterns)
   )
 }
